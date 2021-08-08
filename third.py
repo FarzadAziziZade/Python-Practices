636ff46322c1c40da610903625cc9b0a4856b407
Farzad Azizi Zade <84402243+FarzadAziziZade@users.noreply.github.com>
2021-08-08 20:49:05 +0430

    Add files via upload

---
+++ third.py
@@ -0,0 +1,16 @@
+x=5
+if x==5:
+    print('x is equal to 5')
+if x>4:
+    print('x is greater than 4')
+if x<6:
+    print('x is less than 6')
+if x<=3:
+    print('hey whats wrong with you? oh okay you are just practicing')
+    print('that is fine')
+elif x>=3:
+    print('elif works')
+if x>10:
+    print(' :)))))) ')
+else:
+    print('else works')
