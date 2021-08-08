ea87b9c300ecc9eaa0e210ba669d25af19933239
Farzad Azizi Zade <84402243+FarzadAziziZade@users.noreply.github.com>
2021-08-08 21:44:18 +0430

    Add files via upload

---
+++ sixth.py
@@ -0,0 +1,19 @@
+score = input("Enter Score: ")
+score=float(score)
+if score<=1:
+    if score>=0:
+      if score >= 0.9:
+        print('A')
+      elif score >= 0.8:
+        print('B')
+      elif score >= 0.7:
+        print('C')
+      elif score >= 0.6:
+        print('D')
+      else:
+        print('F')
+    else:
+        print('out of range')
+
+elif score >=1:
+    print('out of range')
